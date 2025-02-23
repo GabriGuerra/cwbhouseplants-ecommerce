21432d3fe6d5be3606a29c9943385d7e8e774594
Gabriel <96262269+GabriGuerra@users.noreply.github.com>
2025-02-22 22:39:20 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://gabriguerra.github.io/cwb-houseplants/',  // Substitua "NOME-DO-REPOSITORIO" pelo nome do seu repositório no GitHub
+  base: 'https://gabriguerra.github.io/cwb-houseplants/',  
 })
