c5c63c554d1658bfd25393549c6df140d8ad9591
GabriGuerra <gb.guerra@icloud.com>
2025-02-24 09:56:19 -0300

    Ajuste no package.json

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', 
+  base: '/', // Garante que os caminhos de build funcionem no Vercel
   build: {
-    outDir: 'dist'
+    outDir: 'dist' // Certifique-se de que o diretório de build esteja configurado como 'dist'
   }
 });
