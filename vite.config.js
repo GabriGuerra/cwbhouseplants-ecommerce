74598f6c8605cc14f68b08f03fe2b9d388d74666
GabriGuerra <gb.guerra@icloud.com>
2025-02-23 21:27:25 -0300

    mudancas para gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://gabriguerra.github.io/cwb-houseplants/',  
+  base: 'https://gabriguerra.github.io/cwbhouseplants-ecommerce/',  
 })
